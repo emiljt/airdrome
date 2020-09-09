a7eed982556c968abef328cf4b28073f5bf435f5
Joshua Terrasas <joshua.terrasas@studentbody.byu.edu>
2020-09-08 21:10:49 -0600

    Start actix server and objects end-point

--- airdrome_api/src/main.rs
+++ airdrome_api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod controllers;
+
+use actix_web::{App, HttpServer, web};
+use controllers::{objects_controller};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/objects"))
+                .route("/", web::get().to(objects_controller::get_objects))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
